7d994877a1f9561d7f09274b6495ae1d77f4deea
Ryan Dikeos <ryan.dikeos@gmail.com>
2019-02-12 12:44:36 -0800

    add + and - grades

---
+++ grading.py
@@ -0,0 +1,28 @@
+num = int(input('enter grade '))
+
+if num < 60:
+    print('\nF')
+elif num < 65:
+    print('\nD-')
+elif num < 68:
+    print('\nD')
+elif num < 70:
+    print('\nD+')
+elif num < 75:
+    print('\nC-')
+elif num < 78:
+    print('\nC')
+elif num < 80:
+    print('\nC+')
+elif num < 85:
+    print('\nB-')
+elif num < 88:
+    print('\nB')
+elif num < 90:
+    print('\nB+')
+elif num < 95:
+    print('\nA-')
+elif num < 97:
+    print('\nA')
+elif num <= 100:
+    print('\nA+')
